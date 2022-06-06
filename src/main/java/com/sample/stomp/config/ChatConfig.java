7171ebac5bc32e6a3a6547ed2dced43504fa7bb9
kiwon kwon <funnykyeon@naver.com>
2022-06-06 17:43:12 +0900

    [fix] Size변경

--- src/main/java/com/sample/stomp/config/ChatConfig.java
+++ src/main/java/com/sample/stomp/config/ChatConfig.java
@@ -26,7 +26,7 @@ public class ChatConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
-        registry.setMessageSizeLimit(500 * 1024 * 1024);
-        registry.setSendBufferSizeLimit(500 * 1024 * 1024);
+        registry.setMessageSizeLimit(512 * 1024 * 1024);
+        registry.setSendBufferSizeLimit(512 * 1024 * 1024);
     }
 }
